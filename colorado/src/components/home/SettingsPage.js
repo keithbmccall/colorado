b3eac79d0cc6c484a882484134a37bd5d94fbfe6
Keith McCall <keithmccall@ool-ad02007f.dyn.optonline.net>
2018-03-15 14:59:19 -0400

    added font library

--- colorado/src/components/home/SettingsPage.js
+++ colorado/src/components/home/SettingsPage.js
@@ -65,7 +65,7 @@ export default class SettingsPage extends Component {
 										{ color: "white" }
 									]}
 								>
-									Compare
+									Viewport
 								</Text>
 
 								<Text style={[styles.text, { color: "white" }]}>

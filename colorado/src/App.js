be932251aa8c63fbd090b47d4557be1dcffb341f
Keith McCall <keithmccall@Keiths-MacBook-Pro.local>
2018-03-17 04:48:30 -0400

    deplying server to heroku

--- colorado/src/App.js
+++ colorado/src/App.js
@@ -20,7 +20,7 @@ import ColorHelper from "color-to-name";
 //
 import { Router } from "./Router";
 console.disableYellowBox = true;
-const SLASH_REQUESTS = "https://3e0e94b9.ngrok.io/api/colorado";
+const SLASH_REQUESTS = "https://e2901d8b.ngrok.ioapi/colorado";
 // const SLASH_REQUESTS = "http://localhost:8080/api/colorado";
 export default class App extends Component {
   constructor() {

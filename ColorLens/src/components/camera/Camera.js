17a15696cbd2a1d1a33e30f46cba2884bc134ed3
keithmccall <keithcodes@gmail.com>
2018-12-15 21:21:49 -0500

    Revamp

---
+++ ColorLens/src/components/camera/Camera.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { View,Text } from "react-native";
+import { RNCamera } from "react-native-camera";
+//
+const Camera = () => (
+//   <RNCamera
+//     type={RNCamera.Constants.Type.back}
+//     flashMode={RNCamera.Constants.FlashMode.on}
+//     permissionDialogTitle={"Permission to use camera"}
+//     permissionDialogMessage={"We need your permission to use your camera phone"}
+//   />
+<View><Text>CAmera text</Text></View>
+);
+
+export default Camera;

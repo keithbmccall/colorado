e31465fafc9c2435f12c47bbfcc31e9b7fa826ea
keithmccall <keithcodes@gmail.com>
2019-01-26 00:41:45 -0500

    added unsplash keys

---
+++ ColorLens/src/keys/index.js
@@ -0,0 +1,12 @@
+const UNSPLASH_KEY = {
+  access: "7c342c7310212d1a1f3fd4e6c1bf6b448719d0ebf2afa2d6d373a5aa1642d518",
+  secret: "0680fecd6d0e017298212313463d118b9a362ce7709b3cb15a1af3c484465a44"
+};
+
+
+
+
+const keychain = {
+    UNSPLASH_KEY
+}
+export default keychain

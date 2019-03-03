e9cd569b3b55b3c10816d5244856b1088b550275
keithmccall <keithcodes@gmail.com>
2019-03-03 03:09:59 -0500

    added imagereader

---
+++ ColorLens/src/shared/containers/color-strips/methods.js
@@ -0,0 +1,22 @@
+import rgb2hex from "rgb2hex";
+
+// buildSwatchObj
+equalizeSwatchLength = swatches => {
+  if (swatches.length < 6) {
+    let n = 6 - swatches.length;
+    for (let i = 0; i < n; i++) {
+      swatches.push(swatches[i]);
+    }
+  }
+  return swatches;
+};
+normalizeSwatches = swatches => {
+  swatches = equalizeSwatchLength(swatches);
+  //
+  return swatches
+    .sort((a, b) => b.population - a.population)
+    .slice(0, 6)
+    .map(swatch => (swatch.color = rgb2hex(swatch.color).hex) && swatch);
+};
+
+module.exports = { normalizeSwatches, equalizeSwatchLength };

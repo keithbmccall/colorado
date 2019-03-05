a27b9e756614e1af0dbbaab346292dfe006d17be
keithmccall <keithcodes@gmail.com>
2019-03-05 09:11:29 -0500

    adding proptypes

--- ColorLens/src/screens/modal/image-studio/components/FocusedImage.js
+++ ColorLens/src/screens/modal/image-studio/components/FocusedImage.js
@@ -1,19 +1,29 @@
 import React from "react";
+import PropTypes from "prop-types";
 import { View } from "react-native";
 import { ColorStripContainer, LoadingView } from "shared/containers";
 import { ResponsiveImage } from "shared/tools";
 import style from "../styles";
 
 //
-const FocusedImage = props => {
-
-  return props.focusedPhoto.valid ? (
-    <View style={{ backgroundColor: "#aaa", height: "50%" }}>
+const FocusedImage = props =>
+  props.focusedPhoto.valid ? (
+    <View style={style.focusedImageWrapper}>
       <ResponsiveImage height="100%" width={1} src={props.focusedPhoto.photo.uri} />
       <ColorStripContainer image={props.focusedPhoto.photo.uri} />
     </View>
   ) : (
     <LoadingView />
   );
+
+// PROPTYPES
+FocusedImage.propTypes = {
+  focusedPhoto: PropTypes.shape({
+    valid: PropTypes.bool.isRequired,
+    photo: PropTypes.shape({
+      uri: PropTypes.string.isRequired
+    })
+  })
 };
 export default FocusedImage;
+
---
+++ ColorLens/src/shared/tools/lists/styles.js
@@ -0,0 +1,18 @@
+import { StyleSheet } from "react-native";
+//
+export default StyleSheet.create({
+    scrollViewContainer: {
+      borderWidth: 10,
+      borderColor: "#000",
+      borderStyle: "solid",
+      flexWrap: "wrap",
+      flexDirection: "row"
+    },
+    flatListContainer: {
+      borderTopWidth: 2,
+      borderLeftWidth: 2,
+      borderRightWidth: 2,
+      borderColor: "#fff",
+      borderStyle: "solid"
+    }
+  });

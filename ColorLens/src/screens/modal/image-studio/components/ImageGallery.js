a2643d2198cc3e27151edab48ff4a47ad20182a1
keithmccall_acadaca <keith.mccall@acadaca.com>
2019-03-06 17:33:16 -0500

    image gallery refactoring

--- ColorLens/src/screens/modal/image-studio/components/ImageGallery.js
+++ ColorLens/src/screens/modal/image-studio/components/ImageGallery.js
@@ -5,23 +5,21 @@ import { LoadingView, ImageWithColorStrip } from "shared/containers";
 import { ScrollableList } from "shared/tools";
 import style from "../styles";
 
-const renderPhotos = props => {
-  const imageCard = (image, key) => {
-    const { rowSize, rowHeight } = props.galleryOptions;
-    const cellSize = { width: `${100 / rowSize}%`, height: rowHeight };
-    return (
-      <TouchableOpacity
-        underlayColor="transparent"
-        style={[style.galleryImageWrapper, cellSize]}
-        onPress={() => props.setFocusedImage(image)}
-        key={key}
-      >
-        <ImageWithColorStrip src={image.uri} />
-      </TouchableOpacity>
-    );
-  };
+const imageCard = (cellSize, image, key) => (
+  <TouchableOpacity
+    underlayColor="transparent"
+    style={[style.galleryImageWrapper, cellSize]}
+    onPress={() => props.setFocusedImage(image)}
+    key={key}
+  >
+    <ImageWithColorStrip src={image.uri} />
+  </TouchableOpacity>
+);
 
-  return props.photos.map(imageCard);
+const renderPhotos = props => {
+  const { rowSize, rowHeight } = props.galleryOptions;
+  const cellSize = { width: `${100 / rowSize}%`, height: rowHeight };
+  return props.photos.map(imageCard.bind(null, cellSize));
 };
 
 const renderContent = props =>

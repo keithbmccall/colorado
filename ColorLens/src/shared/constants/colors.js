3e97002bebd5961e629bcc6724fd1639173a41b6
keithmccall_acadaca <keith.mccall@acadaca.com>
2019-03-07 17:24:36 -0500

    added constant dir (colors & text content)

---
+++ ColorLens/src/shared/constants/colors.js
@@ -0,0 +1,12 @@
+const colors = {
+  black: "black",
+  grey: "#929292",
+  greyDark: "#1F1E1F",
+  greyLight: "#E6E6E6",
+  purple: "#99195E",
+  red: "#E74234",
+  transparent: "transparent",
+  white: "white"
+};
+
+export default colors;

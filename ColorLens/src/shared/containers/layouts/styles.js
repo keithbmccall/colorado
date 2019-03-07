3e5cb02cd9f05319737e2dea57a06a7e2f697b27
keithmccall_acadaca <keith.mccall@acadaca.com>
2019-03-07 14:52:52 -0500

    refactoring

---
+++ ColorLens/src/shared/containers/layouts/styles.js
@@ -0,0 +1,11 @@
+import { StyleSheet } from "react-native";
+//
+
+export default StyleSheet.create({
+  layoutWrapper: {
+    flex: 1,
+    backgroundColor: "#fff",
+    paddingTop: 40,
+    paddingBottom: 30
+  }
+});

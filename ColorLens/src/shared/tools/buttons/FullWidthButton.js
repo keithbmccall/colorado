a59df72731f49d753c10d6ee25b489fe29a87a50
keithmccall <keithcodes@gmail.com>
2019-03-11 09:14:05 -0400

    load state for im w col strp

---
+++ ColorLens/src/shared/tools/buttons/FullWidthButton.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { TouchableOpacity, Button } from "react-native";
+
+const FullWidthButton = props => {
+  return <Button onPress={props.pressMethod} color="#ff0000" title="pr" />;
+};
+export default FullWidthButton;

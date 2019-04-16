4d12a577ba8f8a8aaf43dd026c91a0f1512384e3
keithmccall_acadaca <keith.mccall@acadaca.com>
2019-04-16 12:09:47 -0400

    redux up!

--- ColorLens/src/App.js
+++ ColorLens/src/App.js
@@ -1,15 +1,17 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import Navigation from "./navigation/Router";
-import { Provider, connect } from "react-redux";
+import {Provider} from "react-redux";
+import {initStore} from './store/config'
 
 console.disableYellowBox = true;
 
+const store = initStore();
 export default class App extends Component {
-  render() {
-    return (
-      // <Provider>
-        <Navigation />
-      // </Provider>
-    );
-  }
+    render() {
+        return (
+            <Provider store={store}>
+                <Navigation/>
+            </Provider>
+        );
+    }
 }

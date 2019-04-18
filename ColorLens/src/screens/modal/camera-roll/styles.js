078914bc54c8d113509a4612730532d95a575d4f
keithmccall_acadaca <keith.mccall@acadaca.com>
2019-04-18 18:15:47 -0400

    fullwidthbutton component

---
+++ ColorLens/src/screens/modal/camera-roll/styles.js
@@ -0,0 +1,16 @@
+import {StyleSheet} from "react-native";
+//
+
+export default StyleSheet.create({
+    cameraRollScreenWrapper: {
+        flex: 1,
+
+
+    },
+    titleText: {
+
+    },
+    animatedViewSlider: {
+        position: "absolute"
+    }
+});

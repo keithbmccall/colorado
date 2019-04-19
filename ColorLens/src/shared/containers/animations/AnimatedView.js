9779fecbadac6f4ccc4e897ce13085237ef999b9
keithmccall <keithcodes@gmail.com>
2019-04-19 09:29:01 -0400

    fixing redux

--- ColorLens/src/shared/containers/animations/AnimatedView.js
+++ ColorLens/src/shared/containers/animations/AnimatedView.js
@@ -1,28 +1,42 @@
-import React, { Component } from "react";
-import { Animated } from "react-native";
+import React, {Component} from "react";
+import {Animated} from "react-native";
 
 export default class AnimatedView extends Component {
-  state = {
-    animationValue: new Animated.Value(this.props.animation.starting)
-  };
+    state = {
+        animationValue: new Animated.Value(this.props.animation.starting)
+    };
 
-  openMenu() {
-    Animated.timing(this.state.animationValue, {
-      toValue: this.props.animation.ending,
-      duration: 300
-    }).start();
-  }
-  componentDidUpdate(prevProps) {
-    if (this.props.shouldLaunch !== prevProps.shouldLaunch) {
-      this.openMenu();
+    openMenu = () => {
+        Animated.timing(this.state.animationValue, {
+            toValue: this.props.animation.ending,
+            duration: 300
+        }).start();
     }
-  }
-  render() {
-    const { animationValue } = this.state;
-    return (
-      <Animated.View style={{ ...this.props.style, [this.props.animation.key]: animationValue }}>
-        {this.props.children}
-      </Animated.View>
-    );
-  }
+    closeMenu = () => {
+        Animated.timing(this.state.animationValue, {
+            toValue: this.props.animation.starting,
+            duration: 300
+        }).start();
+    }
+
+    componentDidUpdate(prevProps) {
+        console.log("s",this.props.shouldLaunch,prevProps.shouldLaunch)
+        if (this.props.shouldLaunch !== prevProps.shouldLaunch) {
+            console.log("shoul",this.props.shouldLaunch,prevProps.shouldLaunch)
+            if (this.props.shouldLaunch) {
+                this.openMenu();
+            } else if (!this.props.shouldLaunch) {
+                this.closeMenu();
+            }
+        }
+    }
+
+    render() {
+        const {animationValue} = this.state;
+        return (
+            <Animated.View style={{...this.props.style, [this.props.animation.key]: animationValue}}>
+                {this.props.children}
+            </Animated.View>
+        );
+    }
 }

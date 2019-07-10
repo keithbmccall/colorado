1395010bd1e9f3382b63e9cb5aa32c1c4cd1fed6
keithmccall <keithcodes@gmail.com>
2019-07-09 21:03:35 -0700

    straightening up

--- ColorLens/index.js
+++ ColorLens/index.js
@@ -7,7 +7,7 @@ import {AppRegistry} from 'react-native';
 import App from './src/App';
 import {name as appName} from './app.json';
 import {Provider} from "react-redux";
-import {initStore} from 'store/config'
+import {initStore} from './src/store/config'
 
 const store = initStore();
 

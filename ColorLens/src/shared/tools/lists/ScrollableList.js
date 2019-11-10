df3fdde9c68e8e2ff9ef3d298ecd0743dde0d3f3
keithmccall <keithcodes@gmail.com>
2019-11-09 22:32:22 -0800

    eslint pretteir

--- ColorLens/src/shared/tools/lists/ScrollableList.js
+++ ColorLens/src/shared/tools/lists/ScrollableList.js
@@ -1,26 +1,17 @@
-import React, {Fragment, ReactNode} from "react";
-import {ScrollView, FlatList, StyleSheet} from "react-native";
+import React, { Fragment } from "react";
+import { FlatList, StyleSheet } from "react-native";
 
-// FLATLIST
-type Props = {
-    children: readonly ReactNode[],
-    columns: number,
-    isLazy?: boolean,
-    style?: object
-}
+const ScrollableList = props => (
+  <FlatList
+    key={props.columns === 2 ? 2 : 3}
+    data={props.children}
+    numColumns={props.columns}
+    contentContainerStyle={{ ...style.flatListContainer, ...props.style }}
+    renderItem={({ item }) => <Fragment>{item}</Fragment>}
+  />
+);
 
-//
-const ScrollableList = (props: Props) => (
-    <FlatList
-        key={props.columns === 2 ? 2 : 3}
-        data={props.children}
-        numColumns={props.columns}
-        contentContainerStyle={{...style.flatListContainer, ...props.style}}
-        renderItem={({item}) => <Fragment>{item}</Fragment>}
-    />
-)
-
-ScrollableList.defaultProps = {isLazy: false};
+ScrollableList.defaultProps = { isLazy: false };
 
 //PROPTYPES
 
@@ -28,18 +19,18 @@ export default ScrollableList;
 
 //
 const style = StyleSheet.create({
-    scrollViewContainer: {
-        borderWidth: 10,
-        borderColor: "#000",
-        borderStyle: "solid",
-        flexWrap: "wrap",
-        flexDirection: "row"
-    },
-    flatListContainer: {
-        // borderTopWidth: 2,
-        // borderLeftWidth: 2,
-        // borderRightWidth: 2,
-        // borderColor: "#fff",
-        // borderStyle: "solid"
-    }
+  scrollViewContainer: {
+    borderWidth: 10,
+    borderColor: "#000",
+    borderStyle: "solid",
+    flexWrap: "wrap",
+    flexDirection: "row"
+  },
+  flatListContainer: {
+    // borderTopWidth: 2,
+    // borderLeftWidth: 2,
+    // borderRightWidth: 2,
+    // borderColor: "#fff",
+    // borderStyle: "solid"
+  }
 });
